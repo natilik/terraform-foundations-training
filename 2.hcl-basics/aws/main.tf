3ae8dcfc447d32553f4fe554327da94e126b1651
Mike Guy <mguy@natilik.com>
2022-10-18 20:00:43 +0100

    Adding file_permission to SSH key to accomodate Linux/Mac users with ease.

--- 2.hcl-basics/aws/main.tf
+++ 2.hcl-basics/aws/main.tf
@@ -121,8 +121,9 @@ resource "tls_private_key" "hcl_basics_lab" {
 }
 
 resource "local_file" "hcl_basics_lab" {
-  content  = tls_private_key.hcl_basics_lab.private_key_pem
-  filename = "./vm-private-key.pem"
+  content         = tls_private_key.hcl_basics_lab.private_key_pem
+  file_permission = "600"
+  filename        = "./vm-private-key.pem"
 }
 
 resource "aws_key_pair" "hcl_basics_lab" {
