5bbbeff6d3cd595ec82559a23f6916df2709fac7
Mike Guy <mguy@natilik.com>
2022-12-07 15:59:24 +0000

    Added 7.versioning/azure lab.

---
+++ 7.versioning/azure/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.28.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "versioning_lab" {
+  name     = "rg-versioning-${var.student_name}"
+  location = "uksouth"
+}
+
+resource "azurerm_virtual_network" "versioning_lab" {
+  name                = "vnet-versioning-${var.student_name}"
+  resource_group_name = azurerm_resource_group.versioning_lab.name
+  location            = azurerm_resource_group.versioning_lab.location
+  address_space       = ["10.1.0.0/16"]
+}
+
+resource "azurerm_private_dns_resolver" "versioning_lab" {
+  name                = "pdnsr-versioning-${var.student_name}"
+  resource_group_name = azurerm_resource_group.versioning_lab.name
+  location            = azurerm_resource_group.versioning_lab.location
+  virtual_network_id  = azurerm_virtual_network.versioning_lab.id
+}
---
+++ 7.versioning/instructions.md
@@ -0,0 +1,15 @@
+# Lab - Versioning
+
+1. Navigate to the “7.versioning” directory and select the relevant cloud sub-directory.
+
+2. Your colleague has defined some Terraform resources that they want to create, but they didn’t check the documentation. The old provider version in use doesn’t have all the resources defined. Try an init/plan/apply to see it fail. 
+
+3. Fix the issues in the directory so that the resource successfully deploys. Ensure it deploys with a “terraform apply”.
+
+  * Do this without manually deleting the lockfile file.
+  * Use a “pessimistic versioning” constraint for the version string – any recent version should work.
+
+4. Destroy the infrastructure you just created with a “terraform destroy”.
+
+5. Check the version of terraform you are running, then deliberately set a version constraint to a newer value than you are using. Re-run the plan/apply process and note any error messages (just for familiarisation).
+
