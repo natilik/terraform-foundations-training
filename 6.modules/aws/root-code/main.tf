2613fa16930dc792bc757ab8a740ccfa4aa9c720
Mike Guy <mguy@natilik.com>
2022-12-07 12:10:30 +0000

    Adding missing required_provider blocks.

--- 6.modules/aws/root-code/main.tf
+++ 6.modules/aws/root-code/main.tf
@@ -4,6 +4,18 @@ terraform {
       source  = "hashicorp/aws"
       version = "4.29.0"
     }
+    http = {
+      source  = "hashicorp/http"
+      version = "3.1.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "3.4.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.2.3"
+    }
   }
 }
 
