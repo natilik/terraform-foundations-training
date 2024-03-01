2e5d13706a067ede6b6a7e08fcc8ee69051f0b95
Mike Guy <mguy@natilik.com>
2024-03-01 13:49:56 +0000

    Adding answers for intermediate HCL lab.

--- 8.intermediate_hcl/azure/main.tf
+++ 8.intermediate_hcl/azure/main.tf
@@ -18,8 +18,9 @@ provider "azurerm" {
 }
 
 locals {
-  storage_account_base_name = # Fill me in
-  storage_account_name = substr(replace(local.storage_account_base_name, "-", ""), 0, 20)
+  lowercase_student_name    = lower(var.student_name)
+  storage_account_base_name = "${random_string.storage_account_prefix.result}${local.lowercase_student_name}"
+  storage_account_name      = substr(replace(local.storage_account_base_name, "-", ""), 0, 20)
   common_tags = {
     deployer    = "terraform"
     cost_centre = "abc1234"
@@ -28,37 +29,54 @@ locals {
 }
 
 resource "random_string" "storage_account_prefix" {
-  length  = 10
+  length  = var.random_string_length
   special = false
   upper   = false
 }
 
 resource "azurerm_resource_group" "intermediate_hcl" {
-  name     = "rg-intermediate-hcl-${var.student_name}"
+  name     = "rg-intermediate-hcl-${local.lowercase_student_name}"
   location = "uksouth"
 }
 
 resource "azurerm_storage_account" "intermediate_hcl" {
-  name                     = local.storage_account_name
+  for_each                 = toset(["a", "b"])
+  name                     = "${local.storage_account_name}${each.value}"
   resource_group_name      = azurerm_resource_group.intermediate_hcl.name
   location                 = azurerm_resource_group.intermediate_hcl.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  tags = {
-    "Name" = "${local.storage_account_name}"
-  }
+  tags = merge(
+    local.common_tags,
+    {
+      "Name" = "${local.storage_account_name}${each.value}"
+    }
+  )
 }
 
 # Used in lab part 2, task 3.
-# resource "azurerm_storage_account" "additional_storage_account" {
-#   name                     = "${local.storage_account_name}2"
-#   resource_group_name      = azurerm_resource_group.intermediate_hcl.name
-#   location                 = azurerm_resource_group.intermediate_hcl.location
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-#   tags = merge(
-#     {
-#       "Name" = "${local.storage_account_name}"
-#     }
-#   )
-# }
+resource "azurerm_storage_account" "additional_storage_account" {
+  count                    = var.additional_storage_account_enabled ? 1 : 0
+  name                     = "${local.storage_account_name}2"
+  resource_group_name      = azurerm_resource_group.intermediate_hcl.name
+  location                 = azurerm_resource_group.intermediate_hcl.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  tags = merge(
+    {
+      "Name" = "${local.storage_account_name}"
+    }
+  )
+  blob_properties {
+    dynamic "cors_rule" {
+      for_each = var.cors_rules
+      content {
+        allowed_headers    = cors_rule.value.allowed_headers
+        allowed_methods    = cors_rule.value.allowed_methods
+        allowed_origins    = cors_rule.value.allowed_origins
+        exposed_headers    = cors_rule.value.exposed_headers
+        max_age_in_seconds = cors_rule.value.max_age_in_seconds
+      }
+    }
+  }
+}
--- 8.intermediate_hcl/azure/variables.tf
+++ 8.intermediate_hcl/azure/variables.tf
@@ -9,22 +9,30 @@ variable "student_name" {
   }
 }
 
-variable "environment" {}
+variable "environment" {
+  type = string
+}
+
+variable "random_string_length" {
+  type        = number
+  default     = 10
+  description = "The length of the random string prefix that is used to make up part of the storage account name."
+}
 
 # Used in lab part 2, task 3
-# variable "additional_storage_account_enabled" {
-#   description = "Whether to enable an additional bucket."
-#   type        = bool
-# }
+variable "additional_storage_account_enabled" {
+  description = "Whether to enable an additional bucket."
+  type        = bool
+}
 
 # Used in lab part 2, task 4
-# variable "cors_rules" {
-#   description = "CORS rules for dynamic use in bucket."
-#   type = set(object({
-#     allowed_headers    = list(string)
-#     allowed_methods    = list(string)
-#     allowed_origins    = list(string)
-#     exposed_headers    = list(string)
-#     max_age_in_seconds = number
-#   }))
-# }
+variable "cors_rules" {
+  description = "CORS rules for dynamic use in bucket."
+  type = set(object({
+    allowed_headers    = list(string)
+    allowed_methods    = list(string)
+    allowed_origins    = list(string)
+    exposed_headers    = list(string)
+    max_age_in_seconds = number
+  }))
+}
