190ee735d36a3b47f571582f6ed6503d536dde6a
Mike Guy <mguy@natilik.com>
2024-03-01 09:19:32 +0000

    Removing extra colon from read-host prompts

--- 3.state/azure/scripts/Cleanup-StateLab.ps1
+++ 3.state/azure/scripts/Cleanup-StateLab.ps1
@@ -1,5 +1,5 @@
-$firstName = Read-Host -Prompt 'What is your first name: '
-$surname = Read-Host -Prompt 'What is your surname: '
+$firstName = Read-Host -Prompt 'What is your first name'
+$surname = Read-Host -Prompt 'What is your surname'
 $name = "$firstName$surname".ToLower()
 
 Write-Host "`nDeleting resource group `"rg-state-storage-$name`"..."
